6896c67157e29334add873e746c24638ef5064c7
beatriz-nagae <bh.prestashop@gmail.com>
2025-09-28 03:39:14 -0300

    Create project files

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String nome;
+        String tipoDeConta;
+        boolean negativado;
+        double saldo;
+
+        //menu
+        int opcao = 0;
+
+
+        System.out.println("==============");
+        System.out.print("Qual o nome do cliente? ");
+        nome = scanner.nextLine(); // Lê o nome completo
+
+        System.out.print("Tipo de conta? ");
+        tipoDeConta = scanner.nextLine();
+
+        System.out.print("Saldo Atual? ");
+        saldo = scanner.nextDouble();
+
+        negativado = saldo < 0;
+
+        System.out.println("\n==============");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo de conta: " + tipoDeConta);
+        System.out.println("Saldo Atual: " + saldo);
+        System.out.println("Negativado?: " + (negativado ? "Sim" : "Não"));
+        System.out.println("==============");
+
+        //menu iniciado
+
+        String menu = """
+                **Digite sua opção**
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor
+                4 - Sair
+                """;
+
+        while (opcao != 4){
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+
+            switch(opcao){
+                case 1:
+                    System.out.println("O seu saldo é: " + saldo);
+                    break;
+
+                case 2:
+                    System.out.println("Quanto deseja transferir?");
+                    break;
+
+                case 3:
+                    System.out.println("Quanto recebeu?");
+                    break;
+
+                case 4:
+                    System.out.println("Você saiu.");
+                    break;
+
+                default:
+                    System.out.println("Opção Invalida. Insira de 1-4");
+            }
+
+        }
+
+        scanner.close();
+    }
+}
